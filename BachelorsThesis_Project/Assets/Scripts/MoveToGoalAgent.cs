9495c040c2a0759ffe268d3b607db7ee415c055b
vsRushy <vsRushy@users.noreply.github.com>
2021-02-01 20:28:46 +0100

    Created Training Environment in which a cube finds its target in both x-z directions.

---
+++ BachelorsThesis_Project/Assets/Scripts/MoveToGoalAgent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    [SerializeField]
+    private Transform target_transform;
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        float move_x = vectorAction[0];
+        float move_z = vectorAction[1];
+
+        float move_speed = 5.0f;
+        transform.localPosition += new Vector3(move_x, 0.0f, move_z) * Time.deltaTime * move_speed;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(target_transform.localPosition);
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-4.5f, 1.5f), 1.0f, Random.Range(-4.5f, 4.5f));
+        target_transform.localPosition = new Vector3(Random.Range(-4.5f, 4.5f), 1.0f, Random.Range(-4.5f, 4.5f));
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxisRaw("Horizontal");
+        actionsOut[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1.0f);
+            EndEpisode();
+        }
+    }
+}
