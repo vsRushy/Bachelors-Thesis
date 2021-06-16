96a95de427d911b8c5fd461893c2c09f52317d80
gg1 <gerardmarcosfreixas@gmail.com>
2021-06-17 00:33:05 +0200

    Set up phase 3.

---
+++ BachelorsThesis_Project/Assets/Phase03/Scripts/P3CarAgent.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class P3CarAgent : Agent
+{
+    private P3CarController car_controller;
+
+    [SerializeField]
+    private Checkpoints checkpoints;
+
+    [SerializeField]
+    private Transform spawn_position;
+
+    void Awake()
+    {
+        car_controller = GetComponent<P3CarController>();
+    }
+
+    void Start()
+    {
+        checkpoints.OnCorrectCheckpointEvent += OnCorrectCheckpoint;
+        checkpoints.OnWrongCheckpointEvent += OnWrongCheckpoint;
+    }
+
+    void Update()
+    {
+        Vector3 dir_to_target = (checkpoints.GetNextCheckpoint(transform).transform.position - transform.position).normalized;
+        float velocity_alignment = Vector3.Dot(dir_to_target, car_controller.GetComponent<Rigidbody>().velocity);
+
+        AddReward(1.0f * velocity_alignment * (1.0f / MaxStep));
+        AddReward(car_controller.GetComponent<Rigidbody>().velocity.magnitude * 0.01f);
+    }
+
+    private void OnCorrectCheckpoint(object sender, Checkpoints.CheckpointEventArgs e)
+    {
+        if (e.car_transform == transform)
+        {
+            AddReward(1.0f);
+        }
+    }
+
+    private void OnWrongCheckpoint(object sender, Checkpoints.CheckpointEventArgs e)
+    {
+        if(e.car_transform == transform)
+        {
+            AddReward(-1.0f);
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.position = spawn_position.position + new Vector3(Random.Range(-3.0f, 3.0f), 0, Random.Range(-3.0f, 3.0f));
+        transform.forward = spawn_position.forward;
+        checkpoints.ResetCheckpoint(transform);
+        car_controller.Stop();
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        Vector3 checkpoint_forward = checkpoints.GetNextCheckpoint(transform).transform.forward;
+        float direction_dot = Vector3.Dot(transform.forward, checkpoint_forward);
+        sensor.AddObservation(direction_dot);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float forward_amount = 0.0f;
+        float turn_amount = 0.0f;
+
+        switch(actions.DiscreteActions[0])
+        {
+            case 0: forward_amount = 0.0f; break;
+            case 1: forward_amount = 1.0f; break;
+            case 2: forward_amount = -1.0f; break;
+
+            default: break;
+        }
+
+        switch (actions.DiscreteActions[1])
+        {
+            case 0: turn_amount = 0.0f; break;
+            case 1: turn_amount = 1.0f; break;
+            case 2: turn_amount = -1.0f; break;
+
+            default: break;
+        }
+
+        car_controller.SetInputs(forward_amount, turn_amount);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        int forward_action = 0;
+        if (Input.GetKey(KeyCode.UpArrow)) forward_action = 1;
+        if (Input.GetKey(KeyCode.DownArrow)) forward_action = 2;
+
+        int turn_action = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) turn_action = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) turn_action = 2;
+
+        ActionSegment<int> discrete_actions = actionsOut.DiscreteActions;
+
+        discrete_actions[0] = forward_action;
+        discrete_actions[1] = turn_action;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent<Wall>(out Wall wall))
+        {
+            AddReward(-0.5f);
+            //EndEpisode();
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent<Wall>(out Wall wall))
+        {
+            AddReward(-0.1f);
+        }
+    }
+}
---
+++ BachelorsThesis_Project/Assets/Phase03/Scripts/P3CarController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P3CarController : MonoBehaviour
+{
+    public List<Axle> axles;
+    public float max_motor_torque;
+    public float max_steering_angle;
+    public float max_braque_torque;
+    public float max_speed;
+
+    private Rigidbody rb;
+
+    private float current_speed;
+
+    private float forward_amount;
+    private float turn_amount;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        current_speed = 0.0f;
+    }
+
+    void FixedUpdate()
+    {
+        current_speed = rb.velocity.sqrMagnitude;
+
+        foreach(Axle axle in axles)
+        {
+            if(axle.is_motor)
+            {
+                if (forward_amount < 0.0f)
+                {
+                    rb.drag = 1;
+                }
+                else
+                {
+                    rb.drag = 0;
+                }
+
+                if (current_speed < max_speed)
+                {
+                    axle.left_wheel_collider.motorTorque = forward_amount * max_motor_torque;
+                    axle.right_wheel_collider.motorTorque = forward_amount * max_motor_torque;
+                }
+                else
+                {
+                    axle.left_wheel_collider.motorTorque = 0.0f;
+                    axle.right_wheel_collider.motorTorque = 0.0f;
+                }
+            }
+            
+            if(axle.is_steering)
+            {
+                axle.left_wheel_collider.steerAngle = turn_amount * max_steering_angle;
+                axle.right_wheel_collider.steerAngle = turn_amount * max_steering_angle;
+            }
+
+            UpdateWheelTransform(axle.left_wheel, axle.left_wheel_collider);
+            UpdateWheelTransform(axle.right_wheel, axle.right_wheel_collider);
+        }
+    }
+
+    private void UpdateWheelTransform(GameObject wheel, WheelCollider wheel_collider)
+    {
+        Vector3 position;
+        Quaternion rotation;
+        wheel_collider.GetWorldPose(out position, out rotation);
+
+        wheel.transform.position = position;
+        wheel.transform.rotation = rotation;
+    }
+
+    public void SetInputs(float fa, float ta)
+    {
+        forward_amount = fa;
+        turn_amount = ta;
+    }
+
+    public void Stop()
+    {
+        rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+}
+
+[System.Serializable]
+public class P3Axle
+{
+    public GameObject left_wheel;
+    public GameObject right_wheel;
+
+    public WheelCollider left_wheel_collider;
+    public WheelCollider right_wheel_collider;
+
+    public bool is_motor;
+    public bool is_steering;
+}
